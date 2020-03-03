63e3621b389a6f644dfa1a54e36cb25fbd048ce5
Jamie Gardner <jamiedanielgrdner@gmail.com>
2020-03-03 18:44:40 +0000

    community styles

--- registeredBusiness/community.php
+++ registeredBusiness/community.php
@@ -1,4 +1,3 @@
-     
 <?php
 include ("../connections/conn.php");
 
@@ -13,60 +12,72 @@ $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 ?>
 
 <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="../styles/styles.css"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-<body>
-  
-        
-        
-        <div class="row">
-            
-            <div class="col m1"></div>
-            
-            <div class="col m10 _primary
-                 _noPadding">
-                
-                <div class="row">
-                    <div class="col m3"></div><div class="col m6">
-                        <img src="../pictures/logo.png"/>
-                        </div>
-                    <div class ="col m3"></div>
-                    </div> 
 
-                
-    <ul class='nav'>
-    <div class="ulnav">
-    <li><a href="/bizzconn/registeredBusiness/home.php">Homepage</a></li>
-    <li><a class="active" href="/bizzconn/registeredBusiness/community.php">Community</a></li>
-    <li><a href="/bizzconn/registeredBusiness/editProfile.php">Edit Profile</a></li>
-    <li><a href="/bizzconn/secure/logout.php">Log out</a></li>    
-   
-    </ul>
-                
-                
-         
-                
-                
-<form action="addDiscussion.php" method="post" enctype="multipart/form-data">
-               
-    <legend>Add Discussion:</legend>
-  
-      <div class="row">
-      <div class="col m6">
-      <label>Enter Discussion Title </label>
-      <input name="discussionTitle" type="text" placeholder="Discussion Title" required="required" value="">
-      </div></div>
-      <div class="col m6">
-          <label>Enter Discussion Content</label>
-      <input name="discussionContent" type="text" placeholder="Discussion Content" required="required" value="">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="d-flex justify-content-center my-5">
+        <img src="../pictures/logo.png" />
+
     </div>
-    <div class="col m6">
-          <label>Enter Discussion Category</label>
-      <input name="discussionCategory" type="text" placeholder="Discussion Category" required="required" value="">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+        <ul class='navbar-nav'>
+            <li class="nav-item">
+                <a class="nav-link" href="/bizzconn/registeredBusiness/home.php">Homepage</a></li>
+            <li class="nav-item">
+                <a class="nav-link active" href="/bizzconn/registeredBusiness/community.php">Community</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" class="active" href="/bizzconn/registeredBusiness/editProfile.php">Edit
+                    Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/bizzconn/secure/logout.php">Log out</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container mt-3">
+        <form id="form" class="mx-auto p-3 col-md-10" action="addDiscussion.php" method="post"
+            enctype="multipart/form-data">
+            <div class="form-group row">
+                <legend class="col-form-label col">
+                    <h4>Add Discussion:</h4>
+                </legend>
+            </div>
+            <div class="form-group row">
+                <label class="col-form-label col-sm-4">Enter Discussion Title </label>
+                <div class="col-sm-8">
+                    <input name="discussionTitle" type="text" placeholder="Discussion Title" required="required"
+                        value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-form-label col-sm-4">Enter Discussion Content</label>
+                <div class="col-sm-8">
+                    <input name="discussionContent" type="text" placeholder="Discussion Content" required="required"
+                        value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-form-label col-sm-4">Enter Discussion Category</label>
+                <div class="col-sm-8">
+                    <input name="discussionCategory" type="text" placeholder="Discussion Category" required="required"
+                        value="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-4 mt-3">
+                    <input type="submit" value="Submit Discussion" id="button">
+                </div>
+            </div>
+        </form>
     </div>
-     <input type="submit" value="Submit Discussion" id="button">
-                
-    
-</form>
+</body>
+
+</html>
